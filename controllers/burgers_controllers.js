1f6750337c4d579e86fc8629cd3577759edd528b
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-12 16:38:49 -0600

    heroku deployment

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -37,8 +37,6 @@ module.exports = function (app) {
                 id: burgerId
             }
         }).then(function (dbPost) {
-            console.log('dbPost', dbPost);
-
             res.json(dbPost);
         })
     })
